21ddcc729663c6b5b23e19e6b65295ca6556e52b
Femi Amoo <femi.amoo@payhippo.ng>
2022-02-03 14:02:38 +0000

    local environment config patch

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FILE=.env
-
+FILE_2=config/service-account.json
 cp staging.npmrc .npmrc
 
 echo
@@ -16,7 +16,22 @@ else
 
 fi
 echo
-echo "searching for google application credentials..."
+
+echo "searching for google application credentials json file..."
+
+if [ ! -f "$FILE_2" ]; then
+  echo
+  echo "google application credentials file not found, please add service-account.json file to config directory"
+  echo
+  exit 1
+else
+  echo
+  echo "google application credentials...OK"
+fi
+
+echo
+
+echo "searching for google application credentials environment variable..."
 
 if [[ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]]; then
   echo
