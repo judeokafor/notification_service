38e7f2a0306c72807389d2d917b67f9de54c3d69
Jude Okafor <jude.okafor@payhippo.ng>
2022-03-15 20:08:52 +0100

    [update] template

--- src/templates/sms/reminder.ts
+++ src/templates/sms/reminder.ts
@@ -4,9 +4,9 @@ type Reminder = { businessOwnerName: string; dateDifferenceInDays: number };
 const reminder = (props: Reminder): string => {
 	const { businessOwnerName, dateDifferenceInDays } = props;
 
-	return `Hi ${businessOwnerName},your business loan with Payhippo is due ${
+	return `Hi ${businessOwnerName}, your business loan with Payhippo is due ${
 		dateDifferenceInDays === 1 ? 'tomorrow' : 'in 7 days'
-	}.Sign in ${clientAppUrl} to see adjusted repayment amount as of today.`;
+	}. Sign in ${clientAppUrl} to see adjusted repayment amount as of today.`;
 };
 
 export default reminder;
